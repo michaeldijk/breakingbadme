0ccf8467aac7a817863b121c266c48469f938999
michaeldijk <michaeldijk@outlook.com>
2021-02-23 20:08:25 +0000

    Added comments in charactesrQuoteScript to allow better reading of functions, and understand what does what.

--- assets/js/charactersQuoteScript.js
+++ assets/js/charactersQuoteScript.js
@@ -2,6 +2,7 @@
 let charactersArray = [];
 let quotesArray = [];
 
+// Load quotes from BreakingBadAPI.com, and store them inside QuotesArray to work from later
 function loadQoutes() {
     let xhr = new XMLHttpRequest();
     xhr.open("GET", "https://www.breakingbadapi.com/api/quotes");
@@ -12,7 +13,7 @@ function loadQoutes() {
         }
     };
 }
-
+// Load Characters from BreakingBadAPI.com, and store them inside charactersArray to work from later
 function loadChars() {
     let xhr = new XMLHttpRequest();
     xhr.open("GET", "https://www.breakingbadapi.com/api/characters");
@@ -24,7 +25,9 @@ function loadChars() {
     };
 }
 
+// Function which loads in, if button is pressed to show quote on the site.
 function showQuotes() {
+    // Prevent button from moving browser back to top, as this is default behaviour
     event.preventDefault();
     if (quotesArray.length > 0) {
         randomIndex = Math.floor(Math.random() * quotesArray.length) - 1;
@@ -38,7 +41,9 @@ function showQuotes() {
     }
 };
 
+// Function which loads in, if button is pressed to show Character on the site.
 function showCharacter() {
+    // Prevent button from moving browser back to top, as this is default behaviour
     event.preventDefault();
     if (charactersArray.length > 0) {
         randomIndex = Math.floor(Math.random() * charactersArray.length) - 1;
