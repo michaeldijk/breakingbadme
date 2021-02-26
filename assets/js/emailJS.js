a04f803ec665bb97e90af97e21737475a821535b
michaeldijk <michaeldijk@outlook.com>
2021-02-26 16:58:22 +0000

    made small adjustment in the emailJS script, as it's erroring out on the random number in console.log

--- assets/js/emailJS.js
+++ assets/js/emailJS.js
@@ -5,10 +5,7 @@
 window.onload = function () {
     document.getElementById('contact-form').addEventListener('submit', function (event) {
         event.preventDefault();
-        // generate a five digit number for the contact_number variable
-        this.contact_number.value = Math.random() * 100000 | 0;
-        // these IDs from the previous steps
-        emailjs.sendForm('emailJSOutlook', 'bbEmailTemplate', this)
+        emailjs.sendForm('emailJSOutlook', 'bbEmailTemplate')
             .then(function () {
                 console.log('SUCCESS!');
             }, function (error) {
