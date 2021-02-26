8b2568af2c4dce0cd6349aa3f2c427d02c73695b
michaeldijk <michaeldijk@outlook.com>
2021-02-26 17:08:39 +0000

    adding the random number, to the script, as advised in emailJS documentation

--- assets/js/emailJS.js
+++ assets/js/emailJS.js
@@ -1,6 +1,7 @@
 window.onload = function () {
     document.getElementById('contact-form').addEventListener('submit', function (event) {
         event.preventDefault();
+        this.contact_number.value = Math.random() * 100000 | 0;
         emailjs.sendForm('emailJSOutlook', 'bbEmailTemplate')
             .then(function () {
                 console.log('SUCCESS!');
