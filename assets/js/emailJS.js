746f85507fffd6510ba95eafa9055bba9b681a13
michaeldijk <michaeldijk@outlook.com>
2021-02-26 17:03:40 +0000

    removed emailJS authentication from script, inserted it into body of contact page

--- assets/js/emailJS.js
+++ assets/js/emailJS.js
@@ -1,7 +1,3 @@
-(function () {
-    emailjs.init("user_4QgUIJkPSvelEHJpPosvP");
-})();
-
 window.onload = function () {
     document.getElementById('contact-form').addEventListener('submit', function (event) {
         event.preventDefault();
