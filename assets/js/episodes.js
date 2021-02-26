753ce479d256b33759f00bfc1a75e44844d6662a
michaeldijk <michaeldijk@outlook.com>
2021-02-26 20:06:46 +0000

    added functions for each accordion in the episodes script

--- assets/js/episodes.js
+++ assets/js/episodes.js
@@ -1,5 +1,48 @@
 $(function () {
     $("#accordion").accordion({
-        heightStyle: "content"
+        heightStyle: "content",
+        collapsible: true
+    });
+});
+
+$(function () {
+    $("#accordionSeason1").accordion({
+        heightStyle: "content",
+        collapsible: true
+    });
+});
+
+$(function () {
+    $("#accordionSeason2").accordion({
+        heightStyle: "content",
+        collapsible: true
+    });
+});
+
+$(function () {
+    $("#accordionSeason3").accordion({
+        heightStyle: "content",
+        collapsible: true
+    });
+});
+
+$(function () {
+    $("#accordionSeason4").accordion({
+        heightStyle: "content",
+        collapsible: true
+    });
+});
+
+$(function () {
+    $("#accordionSeason5").accordion({
+        heightStyle: "content",
+        collapsible: true
+    });
+});
+
+$(function () {
+    $("#accordionOthers").accordion({
+        heightStyle: "content",
+        collapsible: true
     });
 });
