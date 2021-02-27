cab163a121cb06a018c2de347f637aa88bf3fa7f
michaeldijk <michaeldijk@outlook.com>
2021-02-27 23:22:32 +0000

    removed the references to localstorage, as it wasn't working, will have to do more testing

--- assets/js/themeswitcher.js
+++ assets/js/themeswitcher.js
@@ -2,10 +2,8 @@ function toggleDarkLightTheme() {
     const el = document.getElementById("styleSheet");
     if (el.href.match("assets/css/darkstyle.css")) {
         el.href = "assets/css/lightstyle.css";
-        localStorage.toggled = "assets/css/lightstyle.css";
 
     } else {
         el.href = "assets/css/darkstyle.css";
-        localStorage.toggled = "";
     }
 }
