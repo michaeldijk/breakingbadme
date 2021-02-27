83e207573587fbbd84891ad9b78a7ce392a3bf2d
michaeldijk <michaeldijk@outlook.com>
2021-02-27 20:49:09 +0000

    Added localstorage, to store variable, permanent

--- assets/js/themeswitcher.js
+++ assets/js/themeswitcher.js
@@ -2,7 +2,10 @@ function toggleDarkLightTheme() {
     const el = document.getElementById("styleSheet");
     if (el.href.match("assets/css/darkstyle.css")) {
         el.href = "assets/css/lightstyle.css";
+        localStorage.toggled = "assets/css/lightstyle.css";
+
     } else {
         el.href = "assets/css/darkstyle.css";
+        localStorage.toggled = "";
     }
 }
