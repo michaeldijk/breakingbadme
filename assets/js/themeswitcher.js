de9d774e1ea917d6a1fee5e73ec76032d3fd68a9
michaeldijk <michaeldijk@outlook.com>
2021-03-06 23:40:14 +0000

    added some more details to git igonore

--- assets/js/themeswitcher.js
+++ assets/js/themeswitcher.js
@@ -1,9 +1,30 @@
-function toggleDarkLightTheme() {
-    const el = document.getElementById("styleSheet");
-    if (el.href.match("assets/css/darkstyle.css")) {
-        el.href = "assets/css/lightstyle.css";
+// default variables, to store dark/light theme, and catch items by IDs
+const lightStoredTheme = "lightstyle";
+const darkStoredTheme = "darkstyle";
+const themeToggle = document.getElementById("themeToggleButton");
+const currentTheme = localStorage.getItem("theme");
+const styleSheet = document.getElementById("cssStyling");
 
+if (currentTheme) {
+    styleSheet.href = "assets/css/" + currentTheme + ".css";
+    if (currentTheme != "lightstyle") {
+        themeToggle.innerHTML = '<i class="fas fa-sun"></i> Light Mode';
+        themeToggle.classList.add("darkModeButton");
     } else {
-        el.href = "assets/css/darkstyle.css";
+        themeToggle.innerHTML = '<i class="fas fa-moon"></i> Dark Mode';
+        themeToggle.classList.remove("darkModeButton");
+    }
+
+}
+
+function toggleDarkLightTheme() {
+    if (styleSheet.href.match("darkstyle.css")) {
+        styleSheet.href = "assets/css/lightstyle.css";
+        themeToggle.innerHTML = '<i class="fas fa-moon"></i> Dark Mode';
+        localStorage.setItem("theme", lightStoredTheme);
+    } else {
+        styleSheet.href = "assets/css/darkstyle.css";
+        themeToggle.innerHTML = '<i class="fas fa-sun"></i> Light Mode';
+        localStorage.setItem("theme", darkStoredTheme);
     }
 }
