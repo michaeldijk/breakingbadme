d582d1ba61fc60fbcc38f0c16104a78e7f7336ba
michaeldijk <michaeldijk@outlook.com>
2021-03-15 12:51:33 +0000

    added script to close menu on button click

---
+++ assets/js/closingMenus.js
@@ -0,0 +1,4 @@
+// Auto-close menu on button click, not needed on other links, as they refer to different pages
+$(".navbar-nav li button").on("click", function () {
+    $(".navbar-collapse").collapse("hide")
+});
